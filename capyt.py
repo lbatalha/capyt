4d0e51868b94cba45687c89f38620761c4da4b7c
lbatalha <lfrb25@gmail.com>
2016-11-14 20:14:39 +0000

    add delete feature

--- capyt.py
+++ capyt.py
@@ -2,26 +2,38 @@
 
 import requests
 import sys, fileinput, argparse, copy
+from urllib.parse import urlparse
 
 url = 'https://cpy.pt/'
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="CLI Utility to interact with pastething")
+
+parser.add_argument('-d', '--delete', help="Delete paste with supplied delete token")
 parser.add_argument('-b', '--burn', type=int, help="Maximum number of paste views before deletion")
 parser.add_argument('-l', '--lexer', help="Lexer to use (pygments get_lexer_by_name)", default="auto")
 parser.add_argument('-t', '--ttl', type=float, help="Time before paste is deleted", default=1.0)
 parser.add_argument('-r', '--raw', help="Wether to have pastebin return raw or html paste url", default='true')
 
-parser.add_argument('FILE', help="Files to Upload", nargs="+")
+parser.add_argument('FILE', help="Files to Upload", nargs="+") #Re-used as paste ID or URL when deleting
 args = vars(parser.parse_args())
 
-parameters = copy.deepcopy(args)
-del parameters['FILE']
+if args['delete']:
+	paste = urlparse(args['FILE'][0])
+	if paste[0] != '':
+		url = args['FILE'][0]
+	else:
+		url = url + paste[2]
+	
+	print("DELETE ", url)
+	r = requests.delete(url, data={"token": args['delete']})
+	print(r.status_code)
 
-with fileinput.input(files=args['FILE'], openhook=fileinput.hook_compressed) as f:
-	parameters['paste'] = ''.join(f)
-	r = requests.post(url, data=parameters)
-	print(r.text)
+else:
+	parameters = copy.deepcopy(args)
+	del parameters['FILE']
 
-
-
+	with fileinput.input(files=args['FILE'], openhook=fileinput.hook_compressed) as f:
+		parameters['paste'] = ''.join(f)
+		r = requests.post(url, data=parameters)
+		print(r.text)
 
