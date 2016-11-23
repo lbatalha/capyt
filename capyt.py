e8feb9b2d417a02295e9bae0d15f4dbd8a2901b8
Laura Batalha <5883822+lbatalha@users.noreply.github.com>
2016-11-23 13:49:06 +0000

    add support for custom file encoding and compression

--- capyt.py
+++ capyt.py
@@ -3,6 +3,7 @@
 import requests
 import sys, fileinput, argparse, copy
 from urllib.parse import urlparse
+from os.path import splitext
 
 url = 'https://cpy.pt/'
 
@@ -13,9 +14,24 @@ parser.add_argument('-b', '--burn', type=int, help="Maximum number of paste view
 parser.add_argument('-l', '--lexer', help="Lexer to use (pygments get_lexer_by_name)", default="auto")
 parser.add_argument('-t', '--ttl', type=float, help="Time in hours before paste expires", default=1.0)
 parser.add_argument('-r', '--raw', help="Weather to have pastebin return raw or html paste url", default='true')
+parser.add_argument('-e', '--encoding', help="Define file encoding for given file (default: utf-8)")
 
 parser.add_argument('FILE', help="Files to upload", nargs="+") #Re-used as paste ID or URL when deleting
 args = vars(parser.parse_args())
+
+#add encoding option to compressed hook
+def hook_compressed_encoded(encoding):
+	def hook_compressed(filename, mode):
+		ext = splitext(filename)[1]
+		if ext == '.gz':
+			import gzip
+			return gzip.open(filename, mode, encoding=encoding)
+		elif ext == '.bz2':
+			import bz2
+			return bz2.BZ2File(filename, mode, encoding=encoding)
+		else:
+			return open(filename, mode, encoding=encoding)
+	return hook_compressed
 
 if args['delete']:
 	paste = urlparse(args['FILE'][0])
@@ -34,7 +50,7 @@ else:
 	parameters = copy.deepcopy(args)
 	del parameters['FILE']
 
-	with fileinput.input(files=args['FILE'], openhook=fileinput.hook_compressed) as f:
+	with fileinput.input(files=args['FILE'], openhook=hook_compressed_encoded(args['encoding'])) as f:
 		parameters['paste'] = ''.join(f)
 		r = requests.post(url, data=parameters)
 		print(r.text)
